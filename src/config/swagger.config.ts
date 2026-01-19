36de21ddae6b66b6cf4bb8a547fdb7802773f9af
paulogmedeiros <paulogsantosm@gmail.com>
2026-01-19 14:40:53 -0300

    feat: swagger

---
+++ src/config/swagger.config.ts
@@ -0,0 +1,16 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export function setupSwagger(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('Webhook Teste')
+    .setDescription(
+      'API desenvolvida para teste, validação e inspeção de webhooks, permitindo simular endpoints, registrar requisições recebidas e analisar payloads em tempo real.',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+}
